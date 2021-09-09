43028de0e7544c3d41762e849c4e92c95e1753b6
zskgit <zskgit@gmail.com>
2021-09-09 18:59:41 +0200

    second file

---
+++ 2021_2022/3GI2Tg1/2_operacje_na_zmiennych.php
@@ -0,0 +1,27 @@
+<?php
+  echo PHP_VERSION; //8.0.10
+  echo 2**10,'<br>';
+
+  $x=1000;
+  $y=100;
+
+  echo $x<=>$y;
+
+// równe / identyczne
+  $x=1;
+  $y=1.0;
+
+  if ($x==$y) {
+    echo "równe";
+  }else{
+    echo "Różne";
+  }
+
+// sprawdzenie dodatkowo typu danych
+  if ($x===$y) {
+    echo "identyczne";
+  }else{
+    echo "Różne";
+  }
+
+ ?>
