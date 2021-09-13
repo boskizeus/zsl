c425bf359f2bd4198bbc79d129a589ec5a5586b3
zskgit <zskgit@gmail.com>
2021-09-13 07:54:52 +0200

    zsl

--- 2021_2022/3GI2Tg1/2_operacje_na_zmiennych.php
+++ 2021_2022/3GI2Tg1/2_operacje_na_zmiennych.php
@@ -24,4 +24,30 @@
     echo "Różne";
   }
 
+/*
+  postinkrementacja $x++
+  preinkrementacja ++$x
+  postdekrementacja $x--
+  predekrementacja --$x
+*/
+
+  $x=1;
+  echo '<hr>',$x; //1
+  echo ++$x; //2
+  echo $x++; //2
+  echo ++$x,'<hr>'; //4
+
+  $x=1;
+  echo $x; //1
+  $x=$x++;
+  echo $x; //1
+  $x=++$x;
+  echo $x; //2
+  $y=++$x;
+  echo $x; //3
+  echo $y; //3
+  $y=$x++;
+  echo $x; //4
+  echo "$y<br>"; //3
+
  ?>
