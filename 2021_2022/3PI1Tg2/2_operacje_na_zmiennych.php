132ac86ff925a8ce292169eae73306d96305a0ee
zskgit <zskgit@gmail.com>
2021-09-16 09:32:32 +0200

    zsl

--- 2021_2022/3PI1Tg2/2_operacje_na_zmiennych.php
+++ 2021_2022/3PI1Tg2/2_operacje_na_zmiennych.php
@@ -30,12 +30,42 @@ $x=1;
 $y=1.0;
 
   if ($x===$y) {
-    echo "Równe";
+    echo "Identyczne";
   }else{
     echo "Różne";
   }
 
   echo gettype($x);
-  echo gettype($y);
+  echo gettype($y).'<hr>';
+
+/*
+  postinkrementacja $x++
+  preinkrementacja ++$x
+  postdekrementacja $x--
+  predekrementacja --$x
+*/
+
+$x=10;
+$x++;
+echo $x; //11
+$x=$x++;
+echo $x; //11
+$y=$x++;
+echo $x; //12
+echo $y; //11
+
+// Zadanie
+$x=1;
+echo $x; //
+$x=$x++;
+echo $x; //
+$x=++$x;
+echo $x; //
+$y=++$x;
+echo $x; //
+echo $y; //
+$y=$x++;
+echo $x; //
+echo $y; //
 
  ?>
