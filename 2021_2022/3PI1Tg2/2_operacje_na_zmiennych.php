39b314a9573c0b5c456bdbc74f43025e034caedf
szyper <31780468+szyper@users.noreply.github.com>
2021-10-01 08:19:20 +0200

    Add files via upload

---
+++ 2021_2022/3PI1Tg2/2_operacje_na_zmiennych.php
@@ -0,0 +1,71 @@
+<?php
+// wersja php
+echo PHP_VERSION; //8.0.10
+
+$pow=2**10;
+echo $pow; //1042
+
+$x=30;
+$y=300;
+
+echo $x<=>$y;
+
+/*
+  komentarz
+  w wielu
+  liniach
+*/
+
+// porównanie oraz identyczność
+$x=1;
+$y=1.0;
+
+  if ($x==$y) {
+    echo "Równe";
+  }else{
+    echo "Różne";
+  }
+
+$x=1;
+$y=1.0;
+
+  if ($x===$y) {
+    echo "Identyczne";
+  }else{
+    echo "Różne";
+  }
+
+  echo gettype($x);
+  echo gettype($y).'<hr>';
+
+/*
+  postinkrementacja $x++
+  preinkrementacja ++$x
+  postdekrementacja $x--
+  predekrementacja --$x
+*/
+
+$x=10;
+$x++;
+echo $x; //11
+$x=$x++;
+echo $x; //11
+$y=$x++;
+echo $x; //12
+echo $y; //11
+
+// Zadanie
+$x=1;
+echo $x; //1
+$x=$x++;
+echo $x; //1
+$x=++$x;
+echo $x; //2
+$y=++$x;
+echo $x; //3
+echo $y; //3
+$y=$x++;
+echo $x; //4
+echo $y; //3
+
+ ?>
---
+++ 2021_2022/3PI1Tg2/scripts/square.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Kwadrat</title>
+  </head>
+  <body>
+    <h3>Kwadrat</h3>
+    <form method="get">
+      <input type="text" name="sideA" placeholder="Podaj długość boku a"><br><br>
+      <input type="submit" name="button" value="Oblicz">
+    </form>
+    <?php
+      if (isset($_GET['button'])) {
+        if (!empty($_GET['sideA'])) {
+          // echo $_GET['sideA'];
+          $sideA=str_replace(',', '.', $_GET['sideA']);
+          // echo $sideA;
+          $area=$sideA**2;
+          $circuit=4*$sideA;
+          echo <<< RESULT
+            <hr>
+            Pole wynosi: $area cm<sup>2</sup><br>
+            Obwód wynosi: $circuit cm
+RESULT;
+        }else {
+          echo "<br>Wypełnij długość boku a";
+        }
+      }
+     ?>
+  </body>
+</html>
